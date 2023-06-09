6ecb5c4279a4f18f29799b59c7245789c4f1a82a
NickStrupat <nick.strupat@gmail.com>
2023-06-08 23:30:50 -0400

    initial code

---
+++ StrupatForm/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Immutable;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using static StrupatFormParser;
+
+const String path = "grammar.sf";
+var text = File.ReadAllText(path);
+var stream = new CodePointCharStream(text) {name = path};
+var lexer = new StrupatFormLexer(stream);
+var tokens = new CommonTokenStream(lexer);
+var parser = new StrupatFormParser(tokens);
+var grammar = parser.grammar_();
+var ruleLookup = grammar.rule_().ToImmutableDictionary(x => x.Name().GetText(), x => x);
+var ruleLookupListener = new Listener(ruleLookup);
+ParseTreeWalker.Default.Walk(ruleLookupListener, grammar);
+;
+
+public partial class StrupatFormParser
+{
+	public partial class Grammar_Context
+	{
+		private ImmutableDictionary<String, Rule_Context>? ruleLookup;
+		public ImmutableDictionary<String, Rule_Context> RuleLookup => ruleLookup ??= rule_().ToImmutableDictionary(x => x.Name().GetText());
+	}
+
+	partial class RuleRefContext
+	{
+		public Rule_Context? GetRuleContext()
+		{
+			var grammarContext = this.FindAncestor<Grammar_Context>();
+			if (grammarContext is null)
+				return null;
+			return grammarContext.RuleLookup.TryGetValue(Name().GetText(), out var rule) ? rule : null;
+		}
+	}
+}
+
+static class ParserRuleExtensions
+{
+	public static TAncestor? FindAncestor<TAncestor>(this RuleContext context) where TAncestor : RuleContext
+	{
+		for (;;)
+			switch (context = context.Parent)
+			{
+				case TAncestor ancestor: return ancestor;
+				case null: return null;
+			}
+	}
+}
+
+sealed class Listener : StrupatFormBaseListener
+{
+	private readonly ImmutableDictionary<String, Rule_Context> ruleLookup;
+	public Listener(ImmutableDictionary<String, Rule_Context> ruleLookup) => this.ruleLookup = ruleLookup;
+
+	public override void EnterRuleRef(RuleRefContext context)
+	{
+		var rule = context.GetRuleContext();
+		if (rule == null)
+			Console.Error.WriteLine($"Rule `{context.Name().GetText()}` referenced at {context.Start.Line}:{context.Start.Column} but not defined");
+		// if (!ruleLookup.TryGetValue(context.Name().GetText(), out var rule))
+		// 	Console.Error.WriteLine($"Rule `{context.Name().GetText()}` referenced at {context.Start.Line}:{context.Start.Column} but not defined");
+	}
+}
---
+++ StrupatForm/StrupatForm.g4
@@ -0,0 +1,51 @@
+grammar StrupatForm;
+
+grammar_: rule_ (Newline+ rule_)* Newline? EOF;
+rule_: Name alternatives;
+alternatives: Space '->' Space alternative | (Newline Indentation alternative)+;
+alternative: item (Space item)*;
+item: (ruleRef | literal | class | '(' alternative ')') quantifier?;
+ruleRef: Name;
+literal: CharLiteral | StringLiteral;
+quantifier
+	: '?' #optional
+	| '*' #zeroOrMore
+	| '+' #oneOrMore
+	| '{' Number '}' #exact
+	| '{' Number ',' '}' #min
+	| '{' Number ',' Number '}' #minMax
+	;
+class
+	: '[' not='^'? range+ ']'
+	| range
+	;
+
+fragment Word: '\\w';
+fragment Digit: '\\d';
+fragment Whitespace: '\\s';
+fragment NotWord: '\\W';
+fragment NotDigit: '\\D';
+fragment NotWhitespace: '\\S';
+ShorthandCharacterClass: Word | Digit | Whitespace | NotWord | NotDigit | NotWhitespace;
+
+//Custom: '[' ~[\]]+ ']';
+Newline: '\r' | '\n' | '\r\n';
+Indentation: '\t' | '    ';
+CharLiteral: '\'' (EscapedCharacter | ~'\'') '\'';
+StringLiteral: '"' (EscapedCharacter | ~'"')* '"';
+fragment EscapedCharacter
+	: '\\' [0\\tnr"']
+	| '\\x' HexadecimalDigit2
+	| '\\u' '{' HexadecimalDigit4 '}'
+	| '\\u' '{' HexadecimalDigit8 '}'
+	;
+fragment HexadecimalDigit: [0-9a-fA-F];
+fragment HexadecimalDigit2: HexadecimalDigit HexadecimalDigit;
+fragment HexadecimalDigit4: HexadecimalDigit2 HexadecimalDigit2;
+fragment HexadecimalDigit8: HexadecimalDigit4 HexadecimalDigit4;
+Name: [\p{Alpha}\p{General_Category=Other_Letter}] [\p{Alnum}\p{General_Category=Other_Letter}]*;
+Space : ' ';
+Number: [\p{Decimal_Number}]+;
+
+range: Character '-' Character | Character | ShorthandCharacterClass;
+Character: EscapedCharacter | ~( '[' | ']' | '-' | '\\' );
